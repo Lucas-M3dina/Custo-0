88d380f781d75843c773933e72c5b25565b7b167
Henrique <henriqueohcos@gmail.com>
2022-02-27 16:09:29 -0300

    Componente card finalizado

--- cust0/src/components/Card.js
+++ cust0/src/components/Card.js
@@ -1,19 +1,27 @@
 import "../css/Home.css"
 import carrinho from "../assets/icoCarrinho.png"
 import foto from "../assets/maguaryUva.png"
+import { Component } from "react"
 
 
-export default function Card(produto) {
-    return (
-        <article className="card-article">
-            <img src={foto} alt="Suco de uva Maguary" />
-            <p>Néctar de Uva Manguary 200ml</p>
-            <span>R$ 1,09 un.</span>
+export default class Card extends Component {
+    render() {
+        var p = this.props.data
+        var real = new Intl.NumberFormat([], { 
+            style: 'currency', 
+            currency: 'BRL' 
+          })
+        return (
+            <article className="card-article">
+                <img src={`data:image;base64,${p.imagem}`} alt="Suco de uva Maguary" />
+                <p>{p.descricao}</p>
+                <span>{real.format(p.preco)} un.</span>
 
-            <button>
-                <img src={carrinho} alt=""/>
-                <p>Adicionar</p>
-            </button>
-        </article>
-    )
+                <button>
+                    <img src={carrinho} alt="" />
+                    Adicionar
+                </button>
+            </article>
+        )
+    }
 }
