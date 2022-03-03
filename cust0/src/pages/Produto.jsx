9b0de6da01a1ab8e0314633b0d356b15a2d9c41a
Henrique Ohnesorge Costa <51917267827@info.scs>
2022-03-03 09:55:24 -0300

    Botão adicionar

--- cust0/src/pages/Produto.jsx
+++ cust0/src/pages/Produto.jsx
@@ -1,11 +1,13 @@
-import { React, Component, useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import nullProduct from "../assets/null.png"
+import carrinho from "../assets/icoCarrinho.png"
 import "../css/Produto.css"
 
 export default function App() {
 
     const [produto, setProduto] = useState({});
+    const [qntade, setQntade] = useState(1);
 
     const real = new Intl.NumberFormat([], {
         style: 'currency',
@@ -13,7 +15,6 @@ export default function App() {
     })
 
     function buscarProduto() {
-
         var url_id = window.location.pathname.split('/')[2]
         // console.log(url)        
 
@@ -24,46 +25,61 @@ export default function App() {
                     setProduto(resposta.data)
                 };
             })
-            .catch(erro => console.log(erro));
+            .catch();
     }
 
+    function postarReserva(event) {
+        event.preventDefault()
+        axios.post('', qntade, produto.id)
+    }
+
+    function onChange(event) {
+        setQntade(event.target.value)
+    }
+
+    function telaProduto() {
+        return (
+            <section onSubmit={postarReserva} className='produto-section'>
+
+                <form className='reservar-produto'>
+                    {
+                        produto.imagem != null ?
+                            /// formato bd
+                            // <img src={`data:image;base64,${p.imagem}`} alt="Imagem produto" /> :
+
+                            /// formato mock
+                            <img src={produto.imagem} alt="Imagem produto" /> :
+
+                            <img src={nullProduct} alt="Imagem produto" />
+                    }
+                    <input type="number" value={qntade} onChange={onChange} min={1} />
+
+                    <button typeof='submit'>
+                        <img src={carrinho} alt="Adicionar" />
+                        Adicionar
+                    </button>
+                </form>
+
+                <div className='info-produto'>
+                    <span className="produto-titulo">{produto.descricao}</span>
+                    <hr />
+                    <span className="produto-preco">{real.format(produto.preco)} un.</span>
+                    <hr />
+                    <span className="produto-descricao-t">Descrição:</span>
+                    <p className="produto-descricao">{produto.description}</p>
+                </div>
+
+            </section>
+        )
+    }
 
     useEffect(buscarProduto, []);
 
-
     return (
         <div>
-
             {
                 produto.id != null ?
-
-                    <section className='produto-section'>
-                        <img src={produto.imagem} alt="Imagem produto" />
-
-                        {/* {
-                            produto.imagem != null ?
-                                /// formato bd
-                                // <img src={`data:image;base64,${p.imagem}`} alt="Imagem produto" /> :
-
-                                /// formato mock
-                                <img src={produto.imagem} alt="Imagem produto" /> :
-
-                                <img src={nullProduct} alt="Imagem produto" />
-                        } */}
-
-                        <div className='info-produto'>
-                            <span className="produto-titulo">{produto.descricao}</span>
-                            <hr />
-                            <span className="produto-preco">{real.format(produto.preco)} un.</span>
-                            <hr />
-                            <span className="produto-descricao-t">Descrição:</span>
-                            <p className="produto-descricao">{produto.description}</p>
-                        </div>
-
-                    </section>
-
-                    :
-
+                    telaProduto() :
                     <p>Não funciona!</p>
             }
         </div>
