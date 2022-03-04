38d4e5c659d4347e3d8354a1f19c553f3b6f97a6
Henrique Ohnesorge Costa <51917267827@info.scs>
2022-03-04 09:10:24 -0300

    Pop-up funcionando

--- cust0/src/pages/Produto.jsx
+++ cust0/src/pages/Produto.jsx
@@ -2,12 +2,16 @@ import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import nullProduct from "../assets/null.png"
 import carrinho from "../assets/icoCarrinho.png"
+
+import edit from "../assets/icon-edit.svg"
+import delet from "../assets/icon-delete.svg"
 import "../css/Produto.css"
 
 export default function App() {
 
     const [produto, setProduto] = useState({});
     const [qntade, setQntade] = useState(1);
+    const [showModal, setShowModal] = useState(false);
 
     const real = new Intl.NumberFormat([], {
         style: 'currency',
@@ -31,6 +35,15 @@ export default function App() {
     function postarReserva(event) {
         event.preventDefault()
         axios.post('', qntade, produto.id)
+    }
+
+    function funcoes() {
+        return (
+            <div className='produto-functions'>
+                <img id="edit" onClick={abrirForm} src={edit} alt="Editar" />
+                <img src={delet} alt="Deletar" />
+            </div>
+        )
     }
 
     function onChange(event) {
@@ -61,6 +74,7 @@ export default function App() {
                 </form>
 
                 <div className='info-produto'>
+                    {funcoes()}
                     <span className="produto-titulo">{produto.descricao}</span>
                     <hr />
                     <span className="produto-preco">{real.format(produto.preco)} un.</span>
@@ -68,9 +82,70 @@ export default function App() {
                     <span className="produto-descricao-t">Descrição:</span>
                     <p className="produto-descricao">{produto.description}</p>
                 </div>
+                {
+                    showModal == true ?
+                    <div className="fundo-pp" onClick={fecharForm}>
+                        <div className="fechar"></div>
+                        <form action="" onClick="stopPropagation">
+                            <h2>Editar Produto</h2>
+
+
+                            <div>
+                                <span>Tipo de produto</span>
+                                <select name="" id="">
+                                    <option value="">1</option>
+                                    <option value="">2</option>
+                                    <option value="">3</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <span>Preço(R$)</span>
+                                <input type="number" name="" id="" />
+                            </div>
+
+                            <div>
+                                <span>Estoque</span>
+                                <input type="number" name="" id="" />
+                            </div>
+
+                            <div>
+                                <span>Imagem</span>
+                                <input type="file" name="" id="" />
+                            </div>
+
+                            <div>
+                                <span>Descrição</span>
+                                <input className="input-descricao-pp" type="text" name="" id="" />
+                            </div>
+
+
+                            <button type="submit">Enviar</button>
+                        </form>
+                    </div> : null
+                }
 
             </section>
         )
+    }
+
+    var modal = document.getElementsByClassName("fundo-pp");
+    var btn = document.getElementById("edit");
+
+    function abrirForm() {
+        setShowModal(true)
+    }
+
+    // span.onclick = function () {
+    //     modal.style.display = "none";
+    // }
+
+    function fecharForm() {
+        setShowModal(false)
+    }
+
+    function stopPropagation(event) {
+        event.stopPropagation();
     }
 
     useEffect(buscarProduto, []);
